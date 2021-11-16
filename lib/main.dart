031186a54a13f5bbb651403edcebb39a96861c15
NourSabry <sabriinour66@gmail.com>
2021-11-16 17:27:32 +0200

    my final commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedM  odeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: MedicalDocumentScreen(),
     );
   }
