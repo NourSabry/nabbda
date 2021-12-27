f54347f4ecc5d01435d7806d39f49b9600336537
Heshamazab1999 <60970771+Heshamazab1999@users.noreply.github.com>
2021-12-27 15:41:26 -0800

    finish last

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nabbda/screens/Home.dart';
-import 'package:nabbda/screens/enter_code_screen.dart';
-import 'package:nabbda/screens/forget_password_screen.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -14,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ForgetPasswordScreen(),
+      home: Home(),
     );
   }
 }
